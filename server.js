23f45ad31948ff4fee5d0b249f54d5f7e8a904b9
david solomon henshaw <david.solomon.henshaw@gmail.com>
2024-05-25 14:41:54 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+
+
+const app = express()
+
+
+
+app.get('/', (req,res) => {
+    res.send("Hello there")
+})
+
+
+app.listen(4000 ,() => {
+    console.log('Server is running')
+})
+
